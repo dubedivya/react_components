f0175baffe82bb6d0cfae97563230e3439544069
Divya Dube <dubedivya1992@gmail.com>
2024-04-25 16:28:40 +0530

    03_StarRating component is completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import "./App.css";
 import RandomColorGenerator from "./components/02_RandomColorGenerator/randomColorGenerator.jsx";
 import Accordion from "./components/01_Accordion/Accordion.jsx";
 import data from "./components/01_Accordion/accordData.js";
+import StarRating from "./components/03_StarRating/index.jsx";
 
 function App() {
   return (
     <>
-      {/*/!*Accordion Component*!/*/}
+   {/*   Accordion Component
       <Accordion data={data} />
 
-      {/*Random Color Generator*/}
-      <RandomColorGenerator />
+      Random Color Generator
+      <RandomColorGenerator />*/}
+
+        {/*Star rating Component*/}
+        <StarRating numberOfStars={10}/>
     </>
   );
 }
